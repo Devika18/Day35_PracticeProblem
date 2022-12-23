3b2889532582f5dec75bee083a3705277ed87472
Devika18 <devikankarpe@gmail.com>
2022-12-24 00:28:18 +0530

    created payroll service database connection

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Payroll Service Database Program");
+        GetConnection connection = new GetConnection();
+        connection.getConnection();
+        connection.closeConnection();
     }
 }
